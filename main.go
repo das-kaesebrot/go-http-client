71678f66527bb414958f69323f87055c482deb3b
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-11-24 17:38:43 +0100

    feature: allow keeping transport channel open

--- main.go
+++ main.go
@@ -57,16 +57,32 @@ func getHttp3Client(f io.Writer) *http3.Transport {
 	return tr
 }
 
+func getHttpClient(keyLogFileWriter io.Writer, httpVersion int) (http.RoundTripper, error) {
+	switch httpVersion {
+	case 1:
+		return getHttp1Client(keyLogFileWriter), nil
+	case 2:
+		return getHttp2Client(keyLogFileWriter), nil
+	case 3:
+		return getHttp3Client(keyLogFileWriter), nil
+	default:
+		return nil, fmt.Errorf("invalid HTTP version: %d", httpVersion)
+	}
+}
+
 func main() {
+	var keepTransport bool
+
 	sslKeyLogFilePath := os.Getenv("SSLKEYLOGFILE")
 	requestUrl := flag.String("url", "https://http3.streaming.ing.hs-rm.de/content/10mb_of_random.img", "The URL to do a GET request against")
 	httpVersion := flag.Int("http", 3, "The HTTP version to use")
 	iterations := flag.Int("iterations", 10, "The amount of iterations to run")
 	outputFile := flag.String("output", "", "The output file to write to (empty is stdout)")
+	flag.BoolVar(&keepTransport, "keep", false, "Keep the underlying transport channel open")
 
 	flag.Parse()
 
-	var keyLogFileWriter io.Writer = io.Discard
+	var keyLogFileWriter io.Writer
 	var outFileWriter io.WriteCloser = os.Stdout
 	var err error
 	var writtenByte int64
@@ -76,40 +92,51 @@ func main() {
 	if sslKeyLogFilePath != "" {
 		keyLogFileWriter, err = os.OpenFile(sslKeyLogFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 		if err != nil {
-			panic(err)
+			log.Fatalf("error: %v", err)
 		}
 	}
 
 	if *outputFile != "" {
 		outFileWriter, err = os.OpenFile(*outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 		if err != nil {
-			panic(err)
+			log.Fatalf("error: %v", err)
 		}
 	}
 
 	defer outFileWriter.Close()
 
+	var tr http.RoundTripper
+	var client *http.Client
+
+	if keepTransport {
+		fmt.Fprint(os.Stderr, "Keeping transport channel open\n")
+		tr, err = getHttpClient(keyLogFileWriter, *httpVersion)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		client = &http.Client{
+			Transport: tr,
+			Timeout:   10 * time.Second,
+		}
+	} else {
+		fmt.Fprint(os.Stderr, "Destroying transport channel on each iteration\n")
+	}
+
 	// yes, we start at 1
 	for i := 1; i <= *iterations; i++ {
-		var tr http.RoundTripper
-
-		switch *httpVersion {
-		case 1:
-			tr = getHttp1Client(keyLogFileWriter)
-		case 2:
-			tr = getHttp2Client(keyLogFileWriter)
-		case 3:
-			tr = getHttp3Client(keyLogFileWriter)
-		default:
-			log.Fatalf("Invalid HTTP version: %d\n", *httpVersion)
+		if !keepTransport {
+			tr, err = getHttpClient(keyLogFileWriter, *httpVersion)
+			if err != nil {
+				log.Fatalf("error: %v", err)
+			}
+			client = &http.Client{
+				Transport: tr,
+				Timeout:   10 * time.Second,
+			}
 		}
 
 		buf := make([]byte, 128*1024) // 128KB buffer
 		counter := &CountingWriter{}
-		client := &http.Client{
-			Transport: tr,
-			Timeout:   10 * time.Second,
-		}
 
 		start := time.Now()
 		resp, err := client.Get(*requestUrl)
@@ -132,12 +159,18 @@ func main() {
 		fmt.Fprintf(os.Stderr, " \033[0K\r [%d/%d] Data: %s (%s)\r", i, *iterations, Binary(writtenByte).String("B"), Decimal(bitrate).String("b/s"))
 
 		measurements = append(measurements, elapsed.Microseconds())
+		if !keepTransport {
+			if closer, ok := tr.(io.Closer); ok {
+				closer.Close()
+			}
+		}
+	}
 
+	if keepTransport {
 		if closer, ok := tr.(io.Closer); ok {
 			closer.Close()
 		}
 	}
-
 	if closer, ok := keyLogFileWriter.(io.Closer); ok {
 		closer.Close()
 	}
